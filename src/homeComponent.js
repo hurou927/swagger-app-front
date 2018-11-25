d11aacab43c5ee8bf4d0877944fa500e5fb00310
Shunji Funasaka <god.be.with.ye.fs@gmail.com>
2018-11-25 21:20:14 +0900

    docs: fix home

--- src/homeComponent.js
+++ src/homeComponent.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-});
-
+const styles = {
+    root: {
+        width: '100%',
+        maxWidth: 500,
+    },
+};
 class Home extends Component {
 
     render(){
+        const { classes } = this.props;
         return (
-            <div style={{margin: '50px'}}>
-            xxxxxxxxxxxxxxxxxx
+            <div style={{ margin: '50px' }} className={classes.root}>
+                <Typography variant="h4" gutterBottom>
+                    HUE Swagger Viewer 
+                </Typography>
+                    <ul>
+                        <Typography variant="body1" gutterBottom>
+                        <li>Update config.yaml when adding new service or deleting exsisting service</li>
+                        <li>Update swagger/[service]/config.yaml when updateing swagger file</li>
+                        </Typography>
+                    </ul>
             </div>
         )
     }
